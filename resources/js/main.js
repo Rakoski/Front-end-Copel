96daec15bcf94f3a2e1f57bc3b3602192feb5dc2
Rakoski <mastrakoski@gmail.com>
2023-09-12 16:25:01 -0300

    Refatoração completa do registro
    feat: Foi feita uma refatoração completa de toda a parte de registro
    de clientes/usuários, isso para podermos acoplar a cada cliente pelo
    menos um cep. Assim, seu id endereço será sua casa/residência e todos
    os clientes que forem cadastrados precisarão informar seu cep a partir
    de agora

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,30 +1,86 @@
 let selectedCep = null;
 let SelectedCepId = null;
 
-function RegistrarEndereco() {
-  let endereco = {}
 
+function getUserIDByEmail(email, callback){
+  $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/api/aluno/encontre/" + email,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (data) {
+        callback(data.idCliente);
+      },
+      error: function (error) {
+        console.error("Erro ao pegar o id do usuário pelo email: " + error);
+      },
+  });
+}
+
+function RegistrarEndereco() {
+
+  function ehUmCepValido(cep) {
+    const cepPattern = /^\d{5}-\d{3}$/;
+    return cepPattern.test(cep);
+  }
+  
+  let endereco = {}
   endereco.cep = $("#cep").val();
   endereco.numero = $("#numero").val();
+  
+  if (!ehUmCepValido(endereco.cep)) {
+    alert("Formato de CEP Inválido!'.");
+    return;
+  }
 
-   $.ajax({
-     type: "POST",
-     url: "http://localhost:8080/api/endereco/registrar",
-     data: JSON.stringify(endereco),
-     contentType: "application/json; charset=utf-8",
-     dataType: "json",
-     success: function (msg) {
-      sessionStorage.setItem('cep', endereco.cep);
-     },
-     error: function (msg) {
-      sessionStorage.setItem('cep', endereco.cep);
-     }
 
-   });
+  if (endereco.numero.length > 5) {
+    alert("Números podem ter até 5 caracteres.");
+    return; 
+  }
+
+  if (endereco.cep && endereco.numero) {
+
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/api/endereco/registrar",
+      data: JSON.stringify(endereco),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (msg) {
+       console.log("sucesso!")
+      },
+      error: function (msg) {
+       console.log("um erro ocorreu no registro do endereço: " + msg)
+      }
+ 
+    });
+
+  } else {
+    console.log("Objeto do endereço não está sendo formado corretamenta.");
+  }
 };
 
+function pegaIdDoEnderecoComBaseNoCep(callback) {
+  const cep = $("#cep").val();
+  const encodedCep = encodeURIComponent(cep);
+
+  $.ajax({
+    type: "GET",
+    url: `http://localhost:8080/api/endereco/encontre-pelo-cep/${encodedCep}`,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function (data) {
+      callback(data);
+    },
+    error: function (error) {
+      console.error("Um erro ocorreu ao pegar o endereço com base no cep:", error);
+    },
+  });
+}
+
+
 function Registrar() {
-  RegistrarEndereco();
 
   let emailpassword = {}
 
@@ -40,15 +96,49 @@ function Registrar() {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (msg) {
-            alert('Sucesso!');
-            window.location.href = "login.html";
+            console.log('Sucesso!');
         },
-        error: function (msg) {
-          window.location.href = "login.html";
+        error: function (error) {
+          console.log("ocorreu um erro no registro do cliente: " + error)
         }
 
     });
 };
+
+function RegistraClienteComSeuEndereco() {
+  RegistrarEndereco();
+  Registrar();
+
+  let emailpassword = {}
+
+  emailpassword.email = $("#email").val();
+
+  getUserIDByEmail(emailpassword.email, function (userId) {
+    pegaIdDoEnderecoComBaseNoCep(function (enderecoIds) {
+        const clienteEndereco = {
+          clienteId: userId,
+          enderecoId: enderecoIds[0], 
+        };
+
+        $.ajax({
+          type: "POST",
+          url: "http://localhost:8080/api/cliente-endereco/registrar",
+          data: JSON.stringify(clienteEndereco),
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          success: function (msg) {
+            console.log('Cliente registrado com sucesso!');
+            window.location.href = 'login.html'
+          },
+          error: function (msg) {
+            console.log('Ocorreu um erro ao registrar o cliente com o endereço.');
+            window.location.href = 'login.html'
+          }
+        });
+    });
+  });
+}
+
 
 function login() {
   var emailpassword = {}
@@ -63,7 +153,6 @@ function login() {
     contentType: "application/json; charset=utf-8",
     dataType: "text",
     success: function (data) {
-      console.log("entrou!")
       sessionStorage.setItem('email', emailpassword.email);
       sessionStorage.setItem('password', emailpassword.password);
       getUserIDByEmail(emailpassword.email, function(userID){
@@ -86,20 +175,22 @@ function logout(){
 
 function validaUser() {
   if (sessionStorage.getItem('validado') != undefined || null) {
-    const email = sessionStorage.getItem('email')
+    const email = sessionStorage.getItem('email');
     getUserInfoByEmail(email, function (userInfo) {
-      $("#setUserName").html('<img src="./resources/img/profile.png" class="profile-icon"> <span class="d-none-md d-inlineblock" style="margin-right: 8px"></span> ' + userInfo.nome + ' ' + userInfo.sobrenome)
+      $("#setUserName").html('<img src="./resources/img/profile.png" class="profile-icon"> <span class="d-none-md d-inlineblock" style="margin-right: 8px"></span> ' + userInfo.nome + ' ' + userInfo.sobrenome);
     });
-    if (sessionStorage.getItem('CepSelectedNumero') == undefined || null) {
+
+    if (sessionStorage.getItem('CepSelectedNumero') == undefined || sessionStorage.getItem('CepSelectedNumero') == null) {
       getUserIDByEmail(email, function (userID) {
         sessionStorage.setItem('ID', userID);
-        getCepsByUserID(userID, function (conta) {
-          setSelectedCep(conta[0]);
-          $("#fodase").html(conta[0] + ' <i class="glyphicon glyphicon-refresh"></i> ');
-        })
+        getCepsByUserID(userID, function (cep) {
+          const selectedCep = cep[0].split(',')[1].trim(); 
+          setSelectedCep(selectedCep);
+          $("#fodase").html(selectedCep + ' <i class="glyphicon glyphicon-refresh"></i> ');
+        });
       });
     } else {
-      const selectedCep = sessionStorage.getItem('CepSelectedNumero');
+      const selectedCep = sessionStorage.getItem('CepSelectedNumero'); 
       $("#fodase").html(selectedCep + ' <i class="glyphicon glyphicon-refresh"></i> ');
     }
   } else {
@@ -108,6 +199,7 @@ function validaUser() {
     return false;
   }
 }
+
 
 // function getQueryParams() {
 //   const params = {};
@@ -139,19 +231,23 @@ function ListarContaDeLuz() {
   const idEndereco = sessionStorage.getItem('E')
 }
 
-function changeCep(curso_nome, fullData) {
+function changeCep(fullData) {
   
   const parts = fullData.split(',');
   const id = parts[0];
   const cep = parts[1];
   const numero = parts[2];
 
-  // Nós só queremos mostrar o cep pra pessoa, caso ela queira muda-lo, não precisa mostrar o id
+  // Nós só queremos mostrar o cep e o número da rua pra pessoa, caso ela queira muda-lo, não precisa mostrar o id
   selectedCep = cep;
   sessionStorage.setItem('CepSelectedNumero', cep);
   sessionStorage.setItem('CepSelectedId', id)
-  sessionStorage.setItem('CepSelectedId', numero)
+  sessionStorage.setItem('CepSelectedIdNumeroRua', numero)
   $("#fodase").html(selectedCep + ' <i class="glyphicon glyphicon-refresh"></i> ');
+}
+
+function AdicionarLinhaTabelaConta(contaData) {
+
 }
 
 function AdicionarLinhaTabela(cepData, addBotao = false) {
@@ -165,7 +261,7 @@ function AdicionarLinhaTabela(cepData, addBotao = false) {
                     '<td>' + cep + '</td>' +
                     '<td>' + numeroEndereco + '</td>';
     if (addBotao) {
-      linhaNova += '<td class="btn-col"><a href="#" onclick="javascript:changeCep(\'' + cep + '\', \'' + fullData + '\');"> Selecionar</a></td>';
+      linhaNova += '<td class="btn-col"><a href="#" onclick="javascript:changeCep(\'' + fullData + '\');"> Selecionar</a></td>';
     }
     linhaNova += '</tr>';
     $("#ListaCadastro tbody").prepend(linhaNova);
@@ -173,16 +269,10 @@ function AdicionarLinhaTabela(cepData, addBotao = false) {
   $('#myModal').modal().hide();
 }
 
-function closeProtocolo(){
-  $( "#openProtocolo" ).removeClass( "show" );
-}
-
 function getSelectedCurso(){
   return {
     "user_id": sessionGet('ID'),
     "curso_id": sessionGet('CepSelectedNumero'),
-    "curso_nome": sessionGet('cursoSelectedNome'),
-    "curso_status": sessionGet('cursoSelectedStatus')
   }
 }
 
@@ -197,23 +287,6 @@ function sessionGet(chave){
   return sessionStorage.getItem(chave)
 }
 
-// Retorna ID do Usuário através do EMAIL
-function getUserIDByEmail(email, callback){
-  $.ajax({
-      type: "GET",
-      url: "http://localhost:8080/api/aluno/encontre/" + email,
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      success: function (data) {
-        callback(data.idCliente);
-      },
-      error: function () {
-        console.error("Error loading1");
-      },
-  });
-}
-
-// Retorna os Dados do Usuário através do EMAIL
 function getUserInfoByEmail(email, callback){
   $.ajax({
       type: "GET",
@@ -236,16 +309,16 @@ function getCepsByUserID(id_user, callback){
       contentType: "application/json; charset=utf-8",
       dataType: "json",
       success: function (data) {
-        const cepData = []; // Array to store the full CEP data
+        const cepData = []; 
   
         for (let i = 0; i < data.length; i++) {
           const parts = data[i].split(','); 
           const cep = parts[1].trim(); 
           const idCep = parts[0].trim();
-          cepData.push(data[i]); // Store the full CEP data
+          cepData.push(data[i]); 
         }
         
-        callback(cepData); // Pass the full CEP data to the callback
+        callback(cepData); 
       },
       error: function () {
         console.error("Error loading2.5");
@@ -256,7 +329,7 @@ function getCepsByUserID(id_user, callback){
 
 function getContaStatusByUserCurso(usuario_id, idConta, callback){
   getCursoUsuarioByUserID(usuario_id, function (conta){
-    for(var i = 0; i < conta.length; i++){
+    for(let i = 0; i < conta.length; i++){
       if(conta[i][1] == idConta){
         callback(conta[i][0])
       }
